7d1add9248576fb35bb3babed8b3fe4995fe75e2
Adrian Martinez <adr23ian@gmail.com>
2017-07-14 22:00:47 -0400

    Implement Devise Signin and Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/', to: 'users/sessions#new'
+    post '/users/sign_in', to: 'users/sessions#create'
+  end
+
 end
